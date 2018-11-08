d6c954cf8f1b1a6925a2f46c7643897be7de7314
GBTC440018UR <GBTC440018UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-11-08 15:55:46 -0500

    added security

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,22 +1,62 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.security.Principal;
+
+
 
 @Controller
 public class HomeController {
     @Autowired
     CourseRepository courseRepository;
 
+
+    @Autowired
+    private UserService userService;
+    @GetMapping("/register")
+    public String showRegistrationPage(Model model) {
+        model.addAttribute("user", new User());
+        return "registration";
+    }
+
+    @PostMapping("/register")
+    public String processRegistrationPage(@Valid @ModelAttribute("user") User user, BindingResult result, Model model){
+        model.addAttribute("user", user);
+        if(result.hasErrors()){
+            return "registration";
+        }
+        else {
+            userService.saveUser(user);
+            model.addAttribute("message", "User Account Created");
+
+        }
+        return "index";
+    }
     @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/secure")
+    public String secure(Principal principal, Model model) {
+        User myuser = (User) ((CustomUserDetails)((UsernamePasswordAuthenticationToken) principal)
+                .getPrincipal()).getUser();
+        model.addAttribute("myuser", myuser);
+        return "list";
+    }
+    @RequestMapping("/list")
     public String listCourses(Model model){
         model.addAttribute("courses", courseRepository.findAll());
         return "list";
@@ -34,7 +74,7 @@ public class HomeController {
             return "courseform";
         }
         courseRepository.save(course);
-        return "redirect:/";
+        return "redirect:/list";
     }
 
     @RequestMapping("/detail/{id}")
@@ -52,6 +92,6 @@ public class HomeController {
     @RequestMapping("/delete/{id}")
     public String delCourse(@PathVariable("id") long id){
         courseRepository.deleteById(id);
-        return "redirect:/";
+        return "redirect:/list";
     }
 }
